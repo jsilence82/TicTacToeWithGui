50f46322857d8fbd939054112a9da4d6593f743d
jeff <jeff.silence@gmail.com>
2026-03-29 21:30:34 +0200

    Remove Factory.java and call PlayerType.create() directly
    Factory was a single-method wrapper that only delegated to PlayerType.create(),
    adding a layer of indirection with no value. GameStateFactory now calls
    PlayerType.create() directly.

--- src/Model/GameStateFactory.java
+++ src/Model/GameStateFactory.java
@@ -1,6 +1,5 @@
 package Model;
 
-import Model.players.Factory;
 import Model.players.Player;
 import Model.players.PlayerType;
 
@@ -9,8 +8,8 @@ public class GameStateFactory {
     public static GameControls createGameState(String player1Name, String player2Name, String player1Type, String player2Type) {
         PlayerType type1 = getPlayerTypeFromString(player1Type);
         PlayerType type2 = getPlayerTypeFromString(player2Type);
-        Player player1 = Factory.playerFactory(type1, player1Name, "X");
-        Player player2 = Factory.playerFactory(type2, player2Name, "O");
+        Player player1 = type1.create(player1Name, "X");
+        Player player2 = type2.create(player2Name, "O");
         return new GameState(player1, player2);
     }
 
--- src/Model/players/Factory.java
+++
@@ -1,7 +0,0 @@
-package Model.players;
-
-public class Factory {
-    public static Player playerFactory(PlayerType playerType, String playersName, String playersMark) {
-        return playerType.create(playersName, playersMark);
-    }
-}
